1c4f9ee7801783de10e0c76098a5ae1ac9ee1c54
MAC <mac@Air-cua-MAC.net.fpt>
2018-04-29 23:21:50 +0700

    init project

---
+++ gameofchat/gameofchat/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  gameofchat
+//
+//  Created by MAC on 4/29/18.
+//  Copyright © 2018 MAC. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
