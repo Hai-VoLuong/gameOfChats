bb6e5c86cbdd30a706ff5ab72296ed71f75d6243
MAC <mac@Air-cua-MAC.net.fpt>
2018-06-21 21:30:26 +0700

    update code and fetch 1 link any ok

--- gameofchat/gameofchat/ViewController.swift
+++ gameofchat/gameofchat/ViewController.swift
@@ -13,57 +13,32 @@ final class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        fetchHomeFeed { (homeFeed) in
-//            print(homeFeed.user)
+//        fetchGenericData(urlString: "https://api.letsbuildthatapp.com/youtube/home_feed") { (homeFeed: HomeFeed) in
 //            homeFeed.videos.forEach({print($0)})
 //        }
-        
-//        fetchDetails { (courseDetails) in
+//
+//        fetchGenericData(urlString: "https://api.letsbuildthatapp.com/youtube/course_detail?id=1" ) { (courseDetails: [CourseDetail]) in
 //            courseDetails.forEach({print($0.name, $0.duration)})
 //        }
         
-        fetchGenericData(urlString: "https://api.letsbuildthatapp.com/youtube/home_feed") { (homeFeed: HomeFeed) in
-            homeFeed.videos.forEach({print($0)})
+        struct Course: Decodable {
+            let id: Int
+            let name: String
+            let link: String
         }
-        
-        fetchGenericData(urlString: "https://api.letsbuildthatapp.com/youtube/course_detail?id=1" ) { (courseDetails: [CourseDetail]) in
-            courseDetails.forEach({print($0.name, $0.duration)})
+        fetchGenericData(urlString: "https://api.letsbuildthatapp.com/jsondecodable/courses") { (courses: [Course]) in
+            courses.forEach({print($0.link)})
         }
     }
-    
-//    fileprivate func fetchHomeFeed(completion: @escaping (HomeFeed) -> ()) {
-//        let urlString = "https://api.letsbuildthatapp.com/youtube/home_feed"
-//        let url = URL(string: urlString)
-//        URLSession.shared.dataTask(with: url!) { (data, resp, err) in
-//            guard let data = data else { return }
-//            do {
-//                let homeFeed = try JSONDecoder().decode(HomeFeed.self, from: data)
-//                completion(homeFeed)
-//            } catch let jsonErr {
-//                print("Failed to decode json:", jsonErr)
-//            }
-//            }.resume()
-//    }
-//
-//    fileprivate func fetchDetails(completion: @escaping ([CourseDetail]) -> ()) {
-//        let urlString = "https://api.letsbuildthatapp.com/youtube/course_detail?id=1"
-//        let url = URL(string: urlString)
-//        URLSession.shared.dataTask(with: url!) { (data, resp, err) in
-//
-//            guard let data = data else { return }
-//
-//            do {
-//                let courseDetails = try JSONDecoder().decode([CourseDetail].self, from: data)
-//                completion(courseDetails)
-//            } catch let jsonErr {
-//                print("Failed to decode json:", jsonErr)
-//            }
-//            }.resume()
-//    }
     
     fileprivate func fetchGenericData<T: Decodable>(urlString: String, completion: @escaping (T) -> ()) {
         let url = URL(string: urlString)
         URLSession.shared.dataTask(with: url!) { (data, resp, err) in
+            
+            if let err = err {
+                print("Failed to fetch data:", err)
+                return
+            }
             
             guard let data = data else { return }
             
